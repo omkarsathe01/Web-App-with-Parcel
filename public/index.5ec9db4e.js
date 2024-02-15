39d73c52fac66221626ecdd658dad737eabf642a
Omkar Sathe <omkarsathe.pers@gmail.com>
2024-02-15 14:39:36 +0530

    initial commit

---
+++ public/index.5ec9db4e.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("button"),t=document.querySelector("body");let o=["darkslateblue","midnightblue","teal","tomato","seagreen","royalblue","saddlebrown","indigo","olivedrab","rosybrown"];e.addEventListener("click",function(){let e=Math.floor(Math.random()*o.length);t.style.setProperty("--bg",o[e])});
+//# sourceMappingURL=index.5ec9db4e.js.map
